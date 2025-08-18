ceec9f371af0e34eb59b4861c34f8983f1e2e33b
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-08-18 17:14:39 +0300

    Filtering the Cards and working with select Element

--- 01-CSS-Wizard-Foundation/building-website-from-scratch/script.js
+++ 01-CSS-Wizard-Foundation/building-website-from-scratch/script.js
@@ -13,3 +13,44 @@ hamburgerButton.addEventListener("click", () => {
         hamburgerButton.setAttribute("aria-expanded", "false");
     }
 });
+
+
+const cards = document.querySelectorAll(".mushroom-guide .card");
+
+const seasonFilter = document.querySelector("#season");
+const edibleFilter = document.querySelector("#edible");
+
+const currentFilters = {
+    season: "all",
+    edible: "all",
+};
+
+seasonFilter.addEventListener("change", updateFilters);
+edibleFilter.addEventListener("change", updateFilters);
+
+function updateFilters(e) {
+    const filterType = e.target.id;
+    currentFilters[filterType] = e.target.value;
+
+    filterCards();
+}
+
+function filterCards(){
+    cards.forEach((card) => {
+        const season = card.querySelector("[data-season]").dataset.season;
+        const edible = card.querySelector("[data-edible]").dataset.edible;
+
+        const matchesSeason = currentFilters.season === season;
+        const matchesEdible = currentFilters.edible === edible;
+
+        if (
+            (currentFilters.season === "all" || currentFilters.season === season) &&
+            (currentFilters.edible === "all" || currentFilters.edible === edible)
+        ) {
+            card.hidden = false;
+        } else {
+            card.hidden = true;
+        }
+
+    })
+}
