4413109e6a36c829718a722163d73d4f9370f1c0
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-08-25 15:59:38 +0300

    Starting With JavaScript

---
+++ 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.1-Arrays-Objects/Script.js
@@ -0,0 +1,14 @@
+/*
+let x;
+
+//Array Literal
+const numbers = [12, 45, 33, 29, 39, 10, 13];
+
+//Array Constructor
+const fruit = new Array("Apple", "Banana", "Orange", "Pear");
+x=numbers[0];
+x= numbers[0] + numbers[3];
+x= `My favorite fruit is ${fruit[3]}`
+x=numbers.length;
+
+console.log(x);*/
