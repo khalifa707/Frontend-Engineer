f1d4f39c2c4f7e1d0e546f4f330a0c4e9b2ee17f
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-08-26 14:40:38 +0300

    Basic Array Methode

--- 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.1-Arrays-Objects/Script.js
+++ 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.1-Arrays-Objects/Script.js
@@ -12,3 +12,20 @@ x= `My favorite fruit is ${fruit[3]}`
 x=numbers.length;
 
 console.log(x);*/
+
+/*
+let x;
+const arr = [1, 2, 3, 4, 5];
+arr.push(6); //add to the end
+arr.unshift(0); //add to the beginning
+arr.pop(); //remove from the end
+arr.shift(); //remove from the beginning
+arr.splice(2, 1); //remove from the middle
+arr.reverse(); //reverse the array
+arr.sort(); //sort the array
+arr.sort((a, b) => a - b); //sort the array in ascending order
+x = arr.includes(4); //check if the array contains a value
+x = arr.indexOf(4); //find the index of a value
+
+console.log(arr);
+console.log(x);*/
