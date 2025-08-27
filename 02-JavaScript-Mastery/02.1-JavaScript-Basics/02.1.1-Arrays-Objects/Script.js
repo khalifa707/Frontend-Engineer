0d7d3240f264c3d0b091e9625145f00ca1691e02
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-08-27 06:39:47 +0300

    Object Spread Operator & Methods

--- 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.1-Arrays-Objects/Script.js
+++ 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.1-Arrays-Objects/Script.js
@@ -71,6 +71,7 @@ arr1.pop(); // This removes the last element (5) from arr1
 x = arr1.concat(arr2); // Now arr1 is [1, 2, 3, 4] and gets concatenated with arr2
 console.log(x);*/
 
+/*
 // Starting With Objects
 const person = {
     name: 'John',
@@ -83,4 +84,41 @@ const person = {
     },
     hobbies: ['reading', 'coding', 'gaming']
 }
-console.log(person.hobbies[0]);
+console.log(person.hobbies[0]);*/
+
+/*
+//Object Spread Operator & Methods
+let x;
+const todo = {};
+todo.id = 1;
+todo.name = "Buy Milk";
+todo.completed = false;
+
+x = todo;
+const person = {
+    address: {
+        coords: {
+            lat: 42.9384,
+            lng: -71.3232
+        }
+    }
+}
+x = person.address.coords.lat;
+
+const obj1 = {a: 1, b: 2};
+const obj2 = {c: 3, d: 4};
+const obj3 = {...obj1, ...obj2};
+
+const todos = [
+    {id: 1, name: 'Buy Milk'},
+    {id: 2, name: 'Buy Bread'},
+    {id: 3, name: 'Buy Eggs'}
+];
+
+x = todos[0].name;
+x= Object.keys(todo).length;
+x= Object.values(todo);
+x= Object.entries(todo);
+x= Object.hasOwnProperty('age');
+
+console.log(x);*/
