f595c087f78d3e5a943dec69a7560a4218f7a3a2
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-09-06 16:11:55 +0300

    Array.filter()

--- 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.1-JavaScript-Mastery-Part1(Basics)/Script.js
+++ 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.1-JavaScript-Mastery-Part1(Basics)/Script.js
@@ -372,3 +372,29 @@ const socials = ['Twitter', 'LinkedIn', 'Facebook', 'Instagram'];
     console.log(item);})*!/
 socials.forEach((item) => console.log(item));
 socials.forEach((item, index, array) => console.log(`${index} - ${item}`));*/
+
+/*
+// Array.filter
+/!*const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = numbers.filter((number) => number % 2 === 0);
+const oddNumbers = numbers.filter((number) => number % 2 !== 0);
+console.log(evenNumbers);
+console.log(oddNumbers);*!/
+
+const companies = [
+    {name: 'Company One', category: 'Finance', start: 1981, end: 2004},
+    {name: 'Company Two', category: 'Retail', start: 1992, end: 2008},
+    {name: 'Company Three', category: 'Auto', start: 1999, end: 2007},
+    {name: 'Company Four', category: 'Retail', start: 1989, end: 2010},
+    {name: 'Company Five', category: 'Technology', start: 2009, end: 2014},
+    {name: 'Company Six', category: 'Finance', start: 1987, end: 2010},
+    {name: 'Company Seven', category: 'Auto', start: 1986, end: 1996},
+    {name: 'Company Eight', category: 'Technology', start: 2011, end: 2016},
+    {name: 'Company Nine', category: 'Retail', start: 1981, end: 1989}
+];
+
+const retailCompanies = companies.filter((company) => company.category === 'Retail');
+console.log(retailCompanies);
+
+const eightiesCompanies = companies.filter((company) => company.start >= 1980 && company.end <= 2005);
+console.log(eightiesCompanies);*/
