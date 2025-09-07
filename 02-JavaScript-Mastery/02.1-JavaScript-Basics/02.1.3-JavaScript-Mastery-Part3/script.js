67d12adb7a38660088bf1065e466b17801b009ef
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-09-07 07:14:50 +0300

    CallBacks

--- 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.3-JavaScript-Mastery-Part3/script.js
+++ 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.3-JavaScript-Mastery-Part3/script.js
@@ -38,3 +38,30 @@ function stopChange() {
 }
 document.getElementById('start').addEventListener('click', startChange);
 document.getElementById('stop').addEventListener('click', stopChange);*/
+
+/*
+//CallBacks
+const posts = [
+    {title: 'Post One', body: 'This is post one'},
+    {title: 'Post Two', body: 'This is post two'},
+];
+
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+function getPosts() {
+    setTimeout(()=> {
+        posts.forEach((post)=>{
+            const div = document.createElement('div');
+            div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+            document.querySelector('.posts').appendChild(div);
+        })
+    });
+}
+
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+getPosts();*/
