a76788256a432ea666a23f8f6da80a7752907384
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-09-07 06:26:36 +0300

    setTimeout & clearTimeout

---
+++ 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.3-JavaScript-Mastery-Part3/script.js
@@ -0,0 +1,14 @@
+// *********** Async JavaScript ***********
+/*
+//setTimeout & clearTimeout
+// setTimeout( changeText, 2000);
+
+function changeText() {
+    document.querySelector('.text').textContent = 'Hello';
+}
+const timerId = setTimeout(changeText, 2000);
+document.querySelector('#stop').addEventListener('click', () => {
+    clearTimeout(timerId);
+    console.log('Timer Cancelled');
+});*/
+
