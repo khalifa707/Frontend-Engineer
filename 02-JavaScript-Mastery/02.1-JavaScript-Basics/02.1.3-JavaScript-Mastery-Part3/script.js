846ee57e2af2ddc8823e018b310c82bea48e1d49
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-09-13 22:19:03 +0300

    Async & Await Error Handling

--- 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.3-JavaScript-Mastery-Part3/script.js
+++ 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.3-JavaScript-Mastery-Part3/script.js
@@ -219,3 +219,36 @@ try {
 } catch (error){
     console.log('Error: ' + error);
 }*/
+
+/*//Async & Await Error Handling
+const getUsers = async () => {
+    try {
+        // const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const response = await fetch('https://httpstat.us/404');
+
+        if(!response.ok){
+            throw new Error(`Request Failed`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log('Error: ' + error);
+    }
+}
+
+const getPosts = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        // const response = await fetch('https://httpstat.us/404');
+
+        if(!response.ok){
+            throw new Error(`Request Failed`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+}
+
+getPosts();
+// getUsers();*/
+
