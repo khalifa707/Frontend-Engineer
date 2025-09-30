000240e835c0bceb6647e44ac8fdf6df23b2c6a9
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-09-30 06:56:21 +0300

    Class Inheritance

--- 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.4-JavaScript-Mastery-part4/script.js
+++ 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.4-JavaScript-Mastery-part4/script.js
@@ -190,3 +190,36 @@ const square = new Rectangle('Square', 20, 20);
 console.log(square.area());
 console.log(square.perimeter());
 console.log(square.isSquare());*/
+
+/*
+//Class Inheritance
+class Shape {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(name, width, height) {
+        super(name);
+        this.width = width;
+        this.height = height;
+    }
+
+    area() {
+        return this.width * this.height;
+    }
+
+    perimeter() {
+        return 2 * (this.width + this.height);
+    }
+
+    isSquare() {
+        return this.width === this.height;
+    }
+}
+
+const square = new Rectangle('Square', 20, 20);
+console.log(square.area());
+console.log(square.perimeter());
+console.log(square.isSquare()); */
