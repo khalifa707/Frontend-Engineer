ec4a08aa1f2cf79db2132adae45975685b19260c
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-10-04 16:27:30 +0300

    Static Method

--- 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.4-JavaScript-Mastery-part4/script.js
+++ 02-JavaScript-Mastery/02.1-JavaScript-Basics/02.1.4-JavaScript-Mastery-part4/script.js
@@ -223,3 +223,32 @@ const square = new Rectangle('Square', 20, 20);
 console.log(square.area());
 console.log(square.perimeter());
 console.log(square.isSquare()); */
+
+/*
+//Static Methods
+class Rectangle {
+    constructor(name, width, height) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+
+    area() {
+        return this.width * this.height;
+    }
+
+    perimeter() {
+        return 2 * (this.width + this.height);
+    }
+
+    isSquare() {
+        return this.width === this.height;
+    }
+
+    static isValidDimensions(width, height) {
+        return width > 0 && height > 0;
+    }
+}
+console.log(Rectangle.isValidDimensions(10, 10));*/
+
+
