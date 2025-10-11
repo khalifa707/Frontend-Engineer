41b3ea1c4387ab56bf27a2fee1e0d2df5cad9bb0
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-10-11 14:38:21 +0300

    Lists

--- 03-React/vite-project/src/components/Rating.jsx
+++ 03-React/vite-project/src/components/Rating.jsx
@@ -2,6 +2,13 @@ const Rating = () => {
     return (
         <div className="rating-container">
            <h2>Rate Your Experience</h2>
+            <div className="starts">
+                {[1,2,3,4,5].map((star) => (
+                    <span key={star} className="star">
+                        &#9733;
+                    </span>
+                ))}
+            </div>
         </div>
     )
 };
