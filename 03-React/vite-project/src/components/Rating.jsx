b2d37ac2f7b32b8a2a7218a1ec6697a30e3c3bb9
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-10-12 07:21:33 +0300

    intro to state

--- 03-React/vite-project/src/components/Rating.jsx
+++ 03-React/vite-project/src/components/Rating.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 const Rating = () => {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
     return (
         <div className="rating-container">
            <h2>Rate Your Experience</h2>
             <div className="starts">
                 {[1,2,3,4,5].map((star) => (
-                    <span key={star} className="star">
-                        &#9733;
+                    <span onClick={() => setRating(star)}
+                          onMouseEnter={() => setHover(star)}
+                          onMouseLeave={() => setHover(0)}
+                          key={star} className="star">
+                        ★
                     </span>
                 ))}
             </div>
