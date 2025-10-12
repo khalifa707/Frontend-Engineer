874018e01d72df154868d5dac66e90a890482056
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-10-12 07:28:47 +0300

    Conditional Rendering Styles

--- 03-React/vite-project/src/components/Rating.jsx
+++ 03-React/vite-project/src/components/Rating.jsx
@@ -3,19 +3,27 @@ import { useState } from 'react';
 const Rating = () => {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
+    const feedbackMessages = [
+        'Terrible',
+        'Bad',
+        'OK',
+        'Good',
+        'Excellent'
+    ];
     return (
         <div className="rating-container">
            <h2>Rate Your Experience</h2>
-            <div className="starts">
+            <div className="stars">
                 {[1,2,3,4,5].map((star) => (
                     <span onClick={() => setRating(star)}
                           onMouseEnter={() => setHover(star)}
                           onMouseLeave={() => setHover(0)}
-                          key={star} className="star">
+                          key={star} className={`star ${star <= (hover || rating) ? 'active' : ''}`}>
                         ★
                     </span>
                 ))}
             </div>
+            {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
         </div>
     )
 };
