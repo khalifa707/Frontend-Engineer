0d3b4be48933782588a388846cd7ab4a050ffc0d
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-10-15 14:28:17 +0300

    Select Inputs Component

--- 03-React/notes-app/src/components/NoteForm.jsx
+++ 03-React/notes-app/src/components/NoteForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import TextInput from "./inputs/TextInput";
+import SelectInput from "./inputs/SelectInput";
 
 const NoteForm = ({notes, setNotes}) => {
 /*    const [title, setTitle] = useState('');
@@ -60,30 +61,28 @@ const NoteForm = ({notes, setNotes}) => {
                         onChange={handleChange}
                         required
                     />
-            <div className="mb-4">
-                <label htmlFor="" className="block font-bold mb-2">Priority</label>
-                <select name="priority"
-                       id="priority"
-                       value={formData.priority}
-                       onChange={handleChange}
-                       className="w-full p-2 border rounded-lg">
-                    <option value="High">🔴 High</option>
-                    <option value="Medium">🟠 Medium</option>
-                    <option value="Low">🟢Low</option>
-                </select>
-            </div>
-            <div className="mb-4">
-                <label htmlFor="" className="block font-bold mb-2">Category</label>
-                <select name="category"
-                       id="category"
-                       value={formData.category}
-                       onChange={handleChange}
-                       className="w-full p-2 border rounded-lg">
-                    <option value="Work">Work</option>
-                    <option value="Personal">Personal</option>
-                    <option value="Ideas">Ideas</option>
-                </select>
-            </div>
+                    <SelectInput
+                        label="Priority"
+                        name="priority"
+                        value={formData.priority}
+                        onChange={handleChange}
+                        options={[
+                            {value: 'Low', label: '🟢 Low'},
+                            {value: 'Medium', label: '🟠 Medium'},
+                            {value: 'High', label: '🔴 High'}
+                        ]}
+                    />
+                    <SelectInput
+                        label="Category"
+                        name="category"
+                        value={formData.category}
+                        onChange={handleChange}
+                        options={[
+                            {value: 'Work', label: 'Work'},
+                            {value: 'Personal', label: 'Personal'},
+                            {value: 'Shopping', label: 'Shopping'}
+                        ]}
+                    />
             <div className="mb-4">
                 <label htmlFor="" className="block font-bold mb-2">Description</label>
                 <textarea name="description"
